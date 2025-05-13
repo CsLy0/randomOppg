75aa6daf326dd5cf1bf5421a042f39ae2506dcb5
CsLy0 <marius.sundt.skullestad@gmail.com>
2025-05-13 13:08:18 +0200

    Add project files.

---
+++ HobbyGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿class generator
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hvorfor vil du ha en ny hobby");
+        string navn = Console.ReadLine();
+        Console.WriteLine($"Hei {navn}, hva er hobbyen din?");
+        string hobby = Console.ReadLine();
+        Console.WriteLine($"Så du liker å {hobby}?");
+    }
+}
+
+
+
---
+++ randomOppg/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Random rand = new Random();
+var randomNumber = rand.Next(0, 3);
+
+if (randomNumber == 0)
+{
+    Console.WriteLine("Tallet ble 0");
+}
+else if (randomNumber == 1)
+{
+    Console.WriteLine("Tallet ble 1");
+}
+else
+{
+    Console.WriteLine("Tallet ble 2");
+}
